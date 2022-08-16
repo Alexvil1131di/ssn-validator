713149970ed11f8e7742267116aa94911d2f0ee6
Oli Japa <olialexander1131@gmail.com>
2022-08-15 23:11:46 -0400

    erase and validate the allowed separator

--- ssn-validator.rb
+++ ssn-validator.rb
@@ -1,4 +1,24 @@
 def ssnValidation(ssn)
+#def local variables 
+
+x=0
+ssn1=""
+ssn2=""
+ssn3=""
+
+#erase the allowed separator
+if ssn.include?("-") && ssn[3] == "-" && ssn[6] == "-"
+    ssn = ssn.delete! "-"
+elsif ssn.include?("-") && ssn[3] != "-" && ssn[6] != "-"
+    puts "Invalid input"
+    return false
+end
+
+if ssn.length != 9 then 
+    puts "Wrong number of characters"
+    return false
+end
+
 
 end
 
